dd77ff562f008854e5a22415fa8c2118313b695c
graceco22 <132722611+graceco22@users.noreply.github.com>
2024-03-09 18:39:21 -0800

    chat bot format

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,18 +1,33 @@
 import React, { useContext } from 'react';
 import { DeepChat } from 'deep-chat-react';
 import DataContext from '../context/dataContext';
+import '../style.css';
 
 const Chatbot = () => {
-    const { showQuiz, question, quizs, checkAnswer, correctAnswer,
-        selectedAnswer,questionIndex, nextQuestion, showTheResult, chatbot }  = useContext(DataContext);
+    const { 
+        showQuiz, 
+        question, 
+        quizs, 
+        checkAnswer, 
+        correctAnswer,
+        selectedAnswer,
+        questionIndex,
+        nextQuestion,
+        showTheResult,
+        chatbot 
+    } = useContext(DataContext);
 
     return (
-         <DeepChat
-                demo={true}
-                style={{ borderRadius: '10px', position:'absolute' }}
+      <div className="Chat">
+        <br></br>
+        <h1>Dino Chat</h1>
+        <DeepChat
+        demo={true}
+        style={{ borderRadius: '10px' }}
         // textInput={{ placeholder: { text: 'Welcome to the demo!' } }}
         // initialMessages={initialMessages}
-      />
+        />
+    </div>
     );
 };
 
